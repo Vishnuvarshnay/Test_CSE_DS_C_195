f2ea7eaf3b1f8affbb5d17896fab6f45b637cc4c
Vishnu Varshnay <121924768+Vishnuvarshnay@users.noreply.github.com>
2024-11-08 14:20:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function calculateTotal() {
+
+function updateTotal() {
   const price = 15;
   const quantity = document.getElementById('quantity').value;
   const total = price * quantity;
   
   document.getElementById('total').textContent = total;
+}
 
-  // Display alert with total cost
+
+function calculateTotal() {
+  const price = 15;
+  const quantity = document.getElementById('quantity').value;
+  const total = price * quantity;
+  
+  
   alert(`You added ${quantity} items to your cart for Rs. ${total}`);
 }
